9dadb17f32d03d87747402f867d0acb121968dad
김서연 <nanemo4168@gmail.com>
2026-08-25 15:38:15 +0900

    공간 확보

--- src/features/finance/pages/FinancePage.vue
+++ src/features/finance/pages/FinancePage.vue
@@ -2962,7 +2962,7 @@ input {
   }
 
   .month-control__label select:last-child {
-    width: 46px;
+    width: 58px;
   }
 
   .view-toggle {
@@ -3209,8 +3209,8 @@ input {
   .category-select select {
     -webkit-appearance: none;
     appearance: none;
-    width: 96px;
-    max-width: 96px;
+    width: 120px;
+    max-width: 120px;
     height: 38px;
     padding: 0 22px 0 10px;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath d='m3 4.5 3 3 3-3' fill='none' stroke='%236b7280' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E") !important;
